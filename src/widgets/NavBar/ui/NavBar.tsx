67f11a555406afc33a326e8cf11ea352240bccb9
Никита <36858449+NikitaTarasenko@users.noreply.github.com>
2023-02-17 17:32:31 +0200

    sidebar links

--- src/widgets/NavBar/ui/NavBar.tsx
+++ src/widgets/NavBar/ui/NavBar.tsx
@@ -1,6 +1,5 @@
 import { useTranslation } from 'react-i18next';
 import { classNames } from 'shared/lib/classNames/classNames';
-import { AppLink, AppLinkTheme } from 'shared/ui/AppLink/AppLink';
 import cls from './NavBar.module.scss';
 
 interface NavBarProps {
@@ -11,10 +10,7 @@ export const NavBar = ({ className }: NavBarProps) => {
 
     return (
         <div className={classNames(cls.NavBar, {}, [className])}>
-            <div className={cls.links}>
-                <AppLink theme={AppLinkTheme.SECONDARY} to="/">{t('Main')}</AppLink>
-                <AppLink theme={AppLinkTheme.SECONDARY} to="/about">{t('About')}</AppLink>
-            </div>
+            /
         </div>
     );
 };
--- src/widgets/Sidebar/ui/Sidebar/Sidebar.tsx
+++ src/widgets/Sidebar/ui/Sidebar/Sidebar.tsx
@@ -1,6 +1,7 @@
 import { useState } from 'react';
 import { useTranslation } from 'react-i18next';
 import { classNames } from 'shared/lib/classNames/classNames';
+import { AppLink, AppLinkTheme } from 'shared/ui/AppLink/AppLink';
 import { Button, SizeButton, ThemeButton } from 'shared/ui/Button/Button';
 import { LangSwitcher } from 'shared/ui/LangSwitcher/LangSwitcher';
 import { ThemeSwitcher } from 'shared/ui/ThemeSwitcher';
@@ -21,6 +22,25 @@ export const Sidebar = ({ className }: SidebarProps) => {
             data-testid="sidebar"
             className={classNames(cls.Sidebar, { [cls.collapsed]: collapsed }, [className])}
         >
+
+            <div className={cls.links}>
+                <AppLink
+                    className={cls.link}
+                    theme={AppLinkTheme.SECONDARY}
+                    to="/"
+                >
+                    {t('Main')}
+                </AppLink>
+                <AppLink
+                    className={cls.link}
+                    theme={AppLinkTheme.SECONDARY}
+                    to="/about"
+                >
+                    {t('About')}
+                </AppLink>
+
+            </div>
+
             <Button
                 className={cls.collapsedBtn}
                 data-testid="sidebarToggle"
@@ -33,7 +53,7 @@ export const Sidebar = ({ className }: SidebarProps) => {
                 {collapsed ? '>' : '<'}
             </Button>
             <div className={cls.switchers}>
-                <ThemeSwitcher />
+                <ThemeSwitcher className={cls.theme} />
                 <LangSwitcher className={cls.lang} />
             </div>
         </div>
